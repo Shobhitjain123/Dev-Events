d1d8df805ab17a30fdeba34393fc6291970c4f71
shobhitjain16 <shobhit.jain@thumpn.com>
2026-04-21 15:35:43 +0530

    Project Setup

---
+++ app/layout.tsx
@@ -0,0 +1,63 @@
+import type { Metadata } from "next";
+import { Schibsted_Grotesk, Martian_Mono, Geist } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import LightRays from "@/components/LightRays";
+
+const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
+
+const schibstedGrotesk = Schibsted_Grotesk({
+  variable: "--font-schibsted-grotesk",
+  subsets: ["latin"],
+});
+
+const martianMono = Martian_Mono({
+  variable: "--font-martian-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Dev Event",
+  description: "The Hub for every Dev Event you must not Miss",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={cn(
+        "min-h-screen",
+        "h-full",
+        "antialiased",
+        schibstedGrotesk.variable,
+        martianMono.variable,
+        "font-sans",
+        geist.variable,
+      )}
+    >
+      <body className="min-h-screen flex flex-col">
+        <div className="absolute inset-0 z-[-1] min-h-screen">
+          <LightRays
+            raysOrigin="top-center-offset"
+            raysColor="#5dfeca"
+            raysSpeed={1}
+            lightSpread={0.5}
+            rayLength={3}
+            followMouse={true}
+            mouseInfluence={0.1}
+            noiseAmount={0}
+            distortion={0}
+            pulsating={false}
+            fadeDistance={1}
+            saturation={1}
+          />
+        </div>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <section>
+      <h1 className="text-center">
+        The Hub for Eveny Dev <br />
+        Event you can't Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackathions, Meetups and Conferences. All in one place
+      </p>
+    </section>
+  );
+}
